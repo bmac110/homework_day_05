eaf9732ba37fb3ca2ba1ec1d86e772ac3d63e522
Billy Mackay <bossmack50@gmail.com>
2019-07-19 14:28:29 +0100

    added increase_pet_sold

---
+++ pet_shop.rb
@@ -0,0 +1,22 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(shop)
+  return shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop, cash)
+  return shop[:admin][:total_cash] += cash
+
+end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, sold)
+  return shop[:admin][:pets_sold] += sold
+end
+
+def stock_count()
